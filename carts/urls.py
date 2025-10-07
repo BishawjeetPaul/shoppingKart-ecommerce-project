87e77bc9ab09518d0ce57d90602a3023a9e51e9f
Bishu Paul <bishawjeet.8809@gmail.com>
2025-10-07 15:14:13 +0530

    modify add to cart function

--- carts/urls.py
+++ carts/urls.py
@@ -5,6 +5,7 @@ from carts import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add/cart/<uuid:product_id>/', views.add_cart, name='add-cart'),
+    path('add/cart/item/<uuid:product_id>/', views.add_cart_item, name='add-cart-item'),
     path('remove/cart/<uuid:product_id>/', views.remove_cart, name='remove-cart'),
     path('remove/cart/item/<uuid:product_id>/', views.remove_cart_item, name='remove-cart-item'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -35,7 +35,26 @@ def add_cart(request, product_id):
         )
         cart_item.save()
     # redirect back to product detail page.
-    # return redirect(reverse('product-details', args=[product.category.slug, product.slug]))
+    return redirect(reverse('product-details', args=[product.category.slug, product.slug]))
+    
+
+
+# This function is use to add-cart-item
+def add_cart_item(request, product_id):
+    product = Product.objects.get(id=product_id) # get the product
+    cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in th session
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1 # cart_item.quantity = cart_item.quantity + 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+        cart_item.save()
     return redirect('cart')
 
 
